b90038c682ee2b2cdda4fff5c67183f7825d4cc7
Madhur Gupta <madhurgupta10@gmail.com>
2019-07-24 20:02:32 +0530

    Fixed Layout and Small Bugs

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -2,16 +2,21 @@ package com.example.weatherapp;
 
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.weatherapp.adapter.CityListAdapter;
+import com.example.weatherapp.model.City;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Bind views using butter knife.
     @BindView(R.id.city_list_view)
     ListView cityListView;
 
@@ -21,10 +26,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        CityListAdapter adapter = new CityListAdapter(this, Cities.getCities());
+        // Fetching the required cities to display the relevant information
+        ArrayList<City> cities = Cities.getCities();
 
-        cityListView = findViewById(R.id.city_list_view);
+        // Check if list is non empty
+        if (cities.size() > 0) {
 
-        cityListView.setAdapter(adapter);
+            CityListAdapter adapter = new CityListAdapter(this, cities);
+
+            cityListView = findViewById(R.id.city_list_view);
+
+            cityListView.setAdapter(adapter);
+
+        } else {
+            Toast.makeText(this, R.string.no_info, Toast.LENGTH_SHORT).show();
+        }
     }
 }
